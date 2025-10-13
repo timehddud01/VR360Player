11d3d2fbb1612eb794c2cc8d0082ec8061b863f4
timehddud <timehddud@gmail.com>
2025-10-13 10:45:46 +0900

    ToMain

--- Assets/Scripts/Video360Play.cs
+++ Assets/Scripts/Video360Play.cs
@@ -7,17 +7,45 @@ public class Video360Play : MonoBehaviour
 {
     VideoPlayer vp;
     public VideoClip[] vcList;
+    int curVCidx;
     // Start is called before the first frame update
     void Start()
     {
         vp=GetComponent<VideoPlayer>();
         vp.clip = vcList[0]; //첫번째 값을 임의로 넣어줌
+        curVCidx = 0; //현재 실행되고 있는 비디오클립의 index를 의미하는 변수 설정
         vp.Play();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   if(Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            // curVCidx = curVCidx -1;
+            // if(curVCidx>=vcList.Length){
+            //     curVCidx = curVCidx + vcList.Length;
+            // }
+
+            curVCidx = (curVCidx -1+vcList.Length)%vcList.Length;
+            
+            
+            vp.clip = vcList[curVCidx];
+    
+        
+        }
+        else if(Input.GetKeyDown(KeyCode.RightBracket))
+        {
+            curVCidx = curVCidx +1;
+            if(curVCidx>=vcList.Length){
+                curVCidx = curVCidx - vcList.Length;
+            }
+            
+            vp.clip = vcList[curVCidx];
+        }
+        
+        
         
     }
+    
+
 }
